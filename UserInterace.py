b288be606bc3a9afa692f228e1a9d684dc3ec24b
August Frisk <114228717+4N0NYM0U5MY7H@users.noreply.github.com>
2023-02-17 04:57:37 -0800

    Update UserInterace.py
    changed Menu class to be abstract.

--- UserInterace.py
+++ UserInterace.py
@@ -1,25 +1,42 @@
 # Author: August Frisk
 # GitHub username: @4N0NYM0U5MY7H
-# Date: 2023, February 15
+# Date: 2023, February 17
 # Description: This file contains all the Menu interface classes
 #              for the CS 361 Book Log program.
 
+r"""User interface menus and options for the CS361 book tracking program.
 
-__version__ = "1.1.0"
+Current menus are:
+    Menu                 Base class for all other Menus.
+    Main Menu            Primary interface for the program.
+    Add Record Menu      Menu for adding new items to the database.
+    Search Records Menu  Menu for searching the database.
+    Delete Record Menu   Menu for removing items from the database.
+"""
+
+from abc import ABC, abstractmethod
+
+__version__ = "1.1.1"
 
 
-class Menu:
-    """Represents a Menu object."""
+class _Menu(ABC):
+    """Base Menu from which all other menus are derived."""
 
+    @abstractmethod
     def __init__(self):
         pass
 
+    @abstractmethod
+    def _menu_header(self):
+        pass
+
+    @abstractmethod
     def display(self):
         pass
 
 
-class MainMenu(Menu):
-    """Represents a Main Menu object."""
+class MainMenu(_Menu):
+    """Main Menu user interface and options."""
 
     def __init__(self):
         super().__init__()
@@ -33,27 +50,26 @@ class MainMenu(Menu):
         }
 
     def _menu_header(self):
-        """Return the Main Menu header."""
+        """Returns a menu header string."""
         header = f"{self._menu_title} -- Please select an Option"
         separator = "-"
         line = f"{separator * (len(header) + 2)}"
         return f"{line}\n{header}\n{line}\n"
 
     def display(self):
-        """Returns the Main Menu."""
-        super().display()
+        """Returns a menu interface string."""
         menu_string = self._menu_header()
         for key in self._menu_options.keys():
             menu_string += f"{key} --- {self._menu_options[key]}\n"
         return menu_string
 
     def get_options(self):
-        """Returns the Main Menu options."""
+        """Returns all options for the current menu."""
         return self._menu_options
 
 
-class AddRecordMenu(Menu):
-    """Represents the Add Record Menu."""
+class AddRecordMenu(_Menu):
+    """Add Record Menu interface and options."""
 
     def __init__(self):
         super().__init__()
@@ -65,14 +81,14 @@ class AddRecordMenu(Menu):
         }
 
     def _menu_header(self):
-        """Returns the Add Record Menu header."""
+        """Returns a menu header string."""
         header = f"{self._menu_title} -- Please enter the following"
         separator = "-"
         line = f"{separator * (len(header) + 2)}"
         return f"{line}\n{header}\n{line}\n"
 
     def display(self):
-        """Returns the Add Record Menu."""
+        """Returns a menu interface string."""
         super().display()
         menu_string = self._menu_header()
         for key in self._menu_options.keys():
@@ -80,8 +96,8 @@ class AddRecordMenu(Menu):
         return menu_string
 
 
-class SearchRecordsMenu(Menu):
-    """Represents the Search Records Menu."""
+class SearchRecordsMenu(_Menu):
+    """Search Records Menu interface and options."""
 
     def __init__(self):
         super().__init__()
@@ -94,27 +110,26 @@ class SearchRecordsMenu(Menu):
         }
 
     def _menu_header(self):
-        """Returns the Search Records Menu header."""
+        """Returns a menu header string."""
         header = f"{self._menu_title} -- Please select an Option"
         separator = "-"
         line = f"{separator * (len(header) + 2)}"
         return f"{line}\n{header}\n{line}\n"
 
     def display(self):
-        """Returns the Search Records Menu."""
-        super().display()
+        """Returns a menu interface string."""
         menu_string = self._menu_header()
         for key in self._menu_options.keys():
             menu_string += f"{key} --- {self._menu_options[key]}\n"
         return menu_string
 
     def get_options(self):
-        """Returns the Search Records Menu options."""
+        """Returns all options for the current menu."""
         return self._menu_options
 
 
-class RemoveRecordMenu(Menu):
-    """Represents the Remove Record Menu."""
+class RemoveRecordMenu(_Menu):
+    """Remove Record Menu interface and options."""
 
     def __init__(self):
         super().__init__()
@@ -127,14 +142,14 @@ class RemoveRecordMenu(Menu):
         }
 
     def _menu_header(self):
-        """Returns the Remove Record Menu header."""
+        """Returns a menu header string."""
         header = f"{self._menu_title} -- Please select an Option"
         separator = "-"
         line = f"{separator * (len(header) + 2)}"
         return f"{line}\n{header}\n{line}\n"
 
     def display(self):
-        """Returns the Remove Record Menu."""
+        """Returns a menu interface string."""
         super().display()
         menu_string = self._menu_header()
         for key in self._menu_options.keys():
@@ -142,5 +157,5 @@ class RemoveRecordMenu(Menu):
         return menu_string
 
     def get_options(self):
-        """Returns the Remove Record Menu options."""
+        """Returns all options for the current menu."""
         return self._menu_options
