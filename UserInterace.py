11e1dcc98337af1d069e4b67ededcc4331ad4f2c
August Frisk <114228717+4N0NYM0U5MY7H@users.noreply.github.com>
2023-02-17 20:32:27 -0800

    User Interface v2.0.0
    Redesign of the UserInterface module.  All menus are now created using the UserInterface class.  The menu titles and options are passed as parameters during instantiation.

--- UserInterace.py
+++ UserInterace.py
@@ -1,27 +1,15 @@
 # Author: August Frisk
 # GitHub username: @4N0NYM0U5MY7H
 # Date: 2023, February 17
-# Description: This file contains all the Menu interface classes
-#              for the CS 361 Book Log program.
 
-r"""User interface menus and options for the CS361 book tracking program.
-
-Current menus are:
-    Menu                 Base class for all other Menus.
-    Main Menu            Primary interface for the program.
-    Add Record Menu      Menu for adding new items to the database.
-    Search Records Menu  Menu for searching the database.
-    Delete Record Menu   Menu for removing items from the database.
-"""
+r"""User interface class representing menus and options for the CS361 book tracking program."""
 
 from abc import ABC, abstractmethod
 
-__version__ = "1.1.1"
+__version__ = "2.0.0"
 
 
 class _Menu(ABC):
-    """Base Menu from which all other menus are derived."""
-
     @abstractmethod
     def __init__(self):
         pass
@@ -34,128 +22,31 @@ class _Menu(ABC):
     def display(self):
         pass
 
+    @abstractmethod
+    def get_options(self):
+        pass
 
-class MainMenu(_Menu):
-    """Main Menu user interface and options."""
 
-    def __init__(self):
+class UserInterface(_Menu):
+    def __init__(self, title, options=None):
         super().__init__()
-        self._menu_title = "Main Menu"
-        self._menu_options = {
-            1: "Add a book to your records",
-            2: "Search for a book",
-            3: "View all books in your records",
-            4: "Remove a book from your records",
-            5: "Exit Program",
-        }
+        self._menu_title = title
+        if options is None:
+            self._menu_options = {}
+        self._menu_options = options
 
     def _menu_header(self):
-        """Returns a menu header string."""
         header = f"{self._menu_title} -- Please select an Option"
         separator = "-"
         line = f"{separator * (len(header) + 2)}"
-        return f"{line}\n{header}\n{line}\n"
+        header_string = f"{line}\n{header}\n{line}\n"
+        return header_string
 
     def display(self):
-        """Returns a menu interface string."""
         menu_string = self._menu_header()
         for key in self._menu_options.keys():
             menu_string += f"{key} --- {self._menu_options[key]}\n"
         return menu_string
 
     def get_options(self):
-        """Returns all options for the current menu."""
-        return self._menu_options
-
-
-class AddRecordMenu(_Menu):
-    """Add Record Menu interface and options."""
-
-    def __init__(self):
-        super().__init__()
-        self._menu_title = "Add Record"
-        self._menu_options = {
-            "Book Title": "Title of the book",
-            "Author Name": "First and last name of the author",
-            "Date Completed": "Date the book was completed",
-        }
-
-    def _menu_header(self):
-        """Returns a menu header string."""
-        header = f"{self._menu_title} -- Please enter the following"
-        separator = "-"
-        line = f"{separator * (len(header) + 2)}"
-        return f"{line}\n{header}\n{line}\n"
-
-    def display(self):
-        """Returns a menu interface string."""
-        super().display()
-        menu_string = self._menu_header()
-        for key in self._menu_options.keys():
-            menu_string += f"{key} --- {self._menu_options[key]}\n"
-        return menu_string
-
-
-class SearchRecordsMenu(_Menu):
-    """Search Records Menu interface and options."""
-
-    def __init__(self):
-        super().__init__()
-        self._menu_title = "Search Records"
-        self._menu_options = {
-            1: "Search by title of the book",
-            2: "Search by author's name",
-            3: "Search by date the book was completed",
-            4: "View all entries",
-        }
-
-    def _menu_header(self):
-        """Returns a menu header string."""
-        header = f"{self._menu_title} -- Please select an Option"
-        separator = "-"
-        line = f"{separator * (len(header) + 2)}"
-        return f"{line}\n{header}\n{line}\n"
-
-    def display(self):
-        """Returns a menu interface string."""
-        menu_string = self._menu_header()
-        for key in self._menu_options.keys():
-            menu_string += f"{key} --- {self._menu_options[key]}\n"
-        return menu_string
-
-    def get_options(self):
-        """Returns all options for the current menu."""
-        return self._menu_options
-
-
-class RemoveRecordMenu(_Menu):
-    """Remove Record Menu interface and options."""
-
-    def __init__(self):
-        super().__init__()
-        self._menu_title = "Remove Record"
-        self._menu_options = {
-            1: "Search by title of the book",
-            2: "Search by author's name",
-            3: "Search by date the book was completed",
-            4: "View all entries",
-        }
-
-    def _menu_header(self):
-        """Returns a menu header string."""
-        header = f"{self._menu_title} -- Please select an Option"
-        separator = "-"
-        line = f"{separator * (len(header) + 2)}"
-        return f"{line}\n{header}\n{line}\n"
-
-    def display(self):
-        """Returns a menu interface string."""
-        super().display()
-        menu_string = self._menu_header()
-        for key in self._menu_options.keys():
-            menu_string += f"{key} --- {self._menu_options[key]}\n"
-        return menu_string
-
-    def get_options(self):
-        """Returns all options for the current menu."""
         return self._menu_options
